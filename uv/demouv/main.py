26783fa990ade0cd5f44a65554bf1f4adf0c6e4d
PrayaasN <prayaas.nadge1996@outlook.com>
2025-07-08 10:43:33 +0530

    Update office scenario message in main function (#5)
    Co-authored-by: Prayaas Nadge <prayaas.nadge1996@outlook.con>

--- uv/demouv/main.py
+++ uv/demouv/main.py
@@ -2,6 +2,6 @@ def main():
     print("Hello from c1")
     print("Hello from c2")
     print("Changes to test office scenarios....")
-
+    print("06072025- Update office scenario message in main function")
 if __name__ == "__main__":
     main()
